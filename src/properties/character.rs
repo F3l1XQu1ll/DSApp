adb9fdfc724534a4f57d271965423a6e0d381a69
FelixQuill <felix-quill@protonmail.com>
2023-07-24 00:23:51 +0200

    Remove leftover files

--- src/properties/character.rs
+++
@@ -1,3 +0,0 @@
-pub const PERSONAL: &'static str = "/character/personal";
-pub const NAME: &'static str = "/character/personal/name";
-pub const GESCHLECHT: &'static str = "/character/personal/geschlecht";
